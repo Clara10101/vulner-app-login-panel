3046f1dead11a33eef54ebcf5858b175cca76c23
Trzcińska Klara <ktrzcins@pentacomp.pl>
2020-06-21 13:46:50 +0200

    Logowanie basic i digest

--- app.py
+++ app.py
@@ -1,58 +1,23 @@
-from flask import Flask, flash, redirect, render_template, \
-     request, url_for
-import os
-from sqlalchemy.orm import sessionmaker
-from passlib.hash import sha256_crypt
-from tabledef import *
-engine = create_engine('sqlite:///tutorial.db', echo=True)
+from flask import Flask
+from flask_httpauth import HTTPDigestAuth
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+auth = HTTPDigestAuth()
+
+users = {
+    "admin": "admin",
+    "tester01": "qwerty",
+    "tester02": "a3b2c1"
+}
+
+@auth.get_password
+def get_pw(username):
+    if username in users:
+        return users.get(username)
+    return None
 
 @app.route('/')
+@auth.login_required
 def index():
-    return render_template('index.html')
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-
-    if request.method == 'POST':
-
-        POST_USERNAME = str(request.form['username'])
-        POST_PASSWORD = str(request.form['password'])
-
-        Session = sessionmaker(bind=engine)
-        s = Session()
-        query_user = s.query(User).filter(User.username.in_([POST_USERNAME]))
-
-        result_user = query_user.first()
-
-        if result_user:
-
-            hash = result_user.password
-
-            if sha256_crypt.verify(POST_PASSWORD, hash):
-                print("Zalogowany")
-                flash('You were successfully logged in')
-                return redirect(url_for('index'))
-        else:
-            sha256_crypt.verify(POST_PASSWORD, "$5$rounds=535000$jp1pVmu1RFmA4c1U$lOitOIXcKjimfwceyqgLHFK1kKfsVGNLb/s3U2pdeKC")
-        error = 'Invalid credentials'
-
-    return render_template('login.html', error=error)
-
-@app.route('/test')
-def test():
-
-    POST_USERNAME = "admin"
-    POST_PASSWORD = "admin"
-
-    Session = sessionmaker(bind=engine)
-    s = Session()
-    query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
-    result = query.first()
-    if result:
-        return "Object found"
-    else:
-        return "Object not found " + POST_USERNAME + " " + POST_PASSWORD
+    return "Hello, %s!" % auth.username()
