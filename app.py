036165989fea4577ec8b41bc601cd7e232c69196
Trzcińska Klara <ktrzcins@pentacomp.pl>
2020-06-23 20:18:39 +0200

    File upload

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 from flask import Flask, flash, redirect, render_template, \
-     request, url_for
+     request, url_for, send_from_directory
 import os
 from sqlalchemy.orm import sessionmaker
 from passlib.hash import sha256_crypt
 from tabledef import *
+from werkzeug.utils import secure_filename
+from zipfile import ZipFile
 engine = create_engine('sqlite:///tutorial.db', echo=True)
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+#file upload configuration
+UPLOAD_FOLDER = ''
+#UPLOAD_FOLDER = '\\uploads'
+#ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'zip'}
+ALLOWED_EXTENSIONS = {'zip'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
@@ -56,3 +66,54 @@ def test():
         return "Object found"
     else:
         return "Object not found " + POST_USERNAME + " " + POST_PASSWORD
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            zip_file = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(zip_file)
+
+            # opening the zip file in READ mode
+            with ZipFile(zip_file, 'r') as zip:
+                # printing all the contents of the zip file
+                zip.printdir()
+
+                # extracting all the files
+                print('Extracting all the files now...')
+                zip.extractall()
+                print('Done!')
+
+            #filename_unzipped =
+
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
